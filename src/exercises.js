b1f69248f70c09330041e07c85777cbecd511fbd
Matthew Hui <matt10145h@gmail.com>
2020-01-08 10:48:28 -0500

    make progress and pulls

--- src/exercises.js
+++ src/exercises.js
@@ -1,66 +1,70 @@
 function commonEnd(a, b) {
-  if (!a || a.length === 0 || !b || b.length === 0) {
-    return false;
-  }
+    if (!a || a.length === 0 || !b || b.length === 0) {
+        return false;
+    }
 
-  if (a[0] === b[0] || a[a.length - 1] === b[b.length - 1]) {
-      return true;
-  } else {
-      return false;
-  }
+    if (a[0] === b[0] || a[a.length - 1] === b[b.length - 1]) {
+          return true;
+    } else {
+          return false;
+    }
 
 }
 
 function endsMeet(values, n) {
-  // write your code here
+    if (!values || values.length < n || n < 0) {
+        return false;
+    }
+
+    // use two for loops to append and preprend
 }
 
 function difference(numbers) {
-  // write your code here
+// write your code here
 }
 
 function max(number) {
-  // write your code here
+// write your code here
 }
 
 function middle(values) {
-  // write your code here
+// write your code here
 }
 
 function increasing(numbers) {
-  // write your code here
+// write your code here
 }
 
 function everywhere(values, x) {
-  // write your code here
+// write your code here
 }
 
 function consecutive(numbers) {
-  // write your code here
+// write your code here
 }
 
 function balance(numbers) {
-  // write your code here
-  console.log("test");
+// write your code here
+console.log("test");
 }
 
 function clumps(values) {
-  // write your code here
+// write your code here
 }
 
 /*
- * Exports all functions for use in external grader.js file. Do not modify.
- */
+* Exports all functions for use in external grader.js file. Do not modify.
+*/
 
 module.exports = {
-  commonEnd,
-  endsMeet,
-  difference,
-  max,
-  middle,
-  increasing,
-  everywhere,
-  consecutive,
-  balance,
-  clumps
+    commonEnd,
+    endsMeet,
+    difference,
+    max,
+    middle,
+    increasing,
+    everywhere,
+    consecutive,
+    balance,
+    clumps
 };
